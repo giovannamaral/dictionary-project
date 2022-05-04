e9703915cbbeff38687d407e0a1b8d80a854c78e
Giovanna <gio30amaral@gmail.com>
2022-05-04 15:08:15 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
-export default function App() {}
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header> Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <a
+            className="text-decoration-none text-secondary"
+            href="https://github.com/giovannamaral/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Giovanna Amaral
+        </footer>
+      </div>
+    </div>
+  );
+}
